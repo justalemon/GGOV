f97336b10af9f850046c349f2d3d5791fb29ab4d
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-23 18:22:21 -0400

    Added more colors for later use

--- Colors.cs
+++ Colors.cs
@@ -5,5 +5,8 @@ namespace GGOHud
     class Colors
     {
         public static Color Background = Color.FromArgb(200, 0, 0, 0);
+        public static Color Healthy = Color.FromArgb(255, 230, 230, 230);
+        public static Color Damaged = Color.FromArgb(255, 247, 227, 18);
+        public static Color NearDeath = Color.FromArgb(255, 200, 0, 0);
     }
 }
