26788522c67d94e3845fa3562e80dbe3974636a7
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-23 13:46:36 -0400

    Added function to calculate offsets

---
+++ Calculations.cs
@@ -0,0 +1,17 @@
+﻿using GTA;
+
+namespace GGOHud
+{
+    class Calculations
+    {
+        public static float CalculateVertical(float Offset)
+        {
+            return Game.ScreenResolution.Height / Offset;
+        }
+
+        public static float CalculateHorizontal(float Offset)
+        {
+            return Game.ScreenResolution.Width / Offset;
+        }
+    }
+}
