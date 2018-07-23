2054783fd9cd96d9eb1b6d95266501e7ad72e5d9
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-23 16:20:54 -0400

    Added name on the HUD

--- Script.cs
+++ Script.cs
@@ -1,20 +1,34 @@
-﻿using GTA;
+﻿using GGOHud;
+using GTA;
 using GTA.Native;
 using System;
+using System.Drawing;
 
 public class GGOHudScript : Script
 {
     public static ScriptSettings Config = ScriptSettings.Load("script\\GGOHud.ini");
+    public static string CharacterName = Game.Player.Name;
 
     public GGOHudScript()
     {
-        Tick += DrawOnTick;
+        Tick += DrawTextOnTick;
+        Tick += DrawShapesOnTick;
         Tick += ChangeOnTick;
+        
+        if (Config.GetValue("GGOHud", "CharacterName", "default") != "default")
+            CharacterName = Config.GetValue("GGOHud", "CharacterName", "default");
     }
 
-    public static void DrawOnTick(object Sender, EventArgs Event)
+    public static void DrawTextOnTick(object Sender, EventArgs Event)
     {
-        
+        UIText CharacterNameHUD = new UIText(CharacterName, Calculations.CalculatePoint(80.1f, 77.5f), 0.325f);
+        CharacterNameHUD.Draw();
+    }
+
+    public static void DrawShapesOnTick(object Sender, EventArgs Event)
+    {
+        UIRectangle CharacterBackground = new UIRectangle(Calculations.CalculatePoint(80, 77), Calculations.CalculateSize(5, 15), Color.Black);
+        CharacterBackground.Draw();
     }
 
     public static void ChangeOnTick(object Sender, EventArgs Event)
