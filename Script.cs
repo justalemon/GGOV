a6ecb2f4ce6f86aa754c49b49ea8da7d5265c01a
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-25 14:01:10 -0400

    Improved weapon detection

--- Script.cs
+++ Script.cs
@@ -2,7 +2,6 @@
 using GTA;
 using GTA.Native;
 using System;
-using System.IO;
 
 public class GGOHudScript : Script
 {
@@ -27,23 +26,23 @@ public class GGOHudScript : Script
 
     public static void DrawTextOnTick(object Sender, EventArgs Event)
     {
-        // First Row - Player name - Example: LLENN
+        // Player name - Example: LLENN
         Draw.Text(CharacterName, Position.PlayerName, 0.325f, false);
 
         // Draw dummies if the weapon should not be shown
-        if (Checks.IsCurrentWeaponBanned())
+        if (Weapons.CurrentType() == Weapons.Type.Banned)
         {
             Draw.Dummy(Position.PrimaryAmmoDummy);
             Draw.Dummy(Position.SecondaryAmmoDummy);
         }
-        // Second Row - Ammo for primary weapons - Example: 30 (for the Bullpup Rifle)
-        else if (!Checks.IsCurrentWeaponSidearm())
+        // Ammo for primary weapons - Example: 30 (for the Bullpup Rifle)
+        else if (Weapons.CurrentType() == Weapons.Type.Main)
         {
             Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), Position.PrimaryAmmo);
             Draw.Dummy(Position.SecondaryAmmoDummy);
         }
-        // Third Row - Ammo for sidearms - Example: 9 (for the .50 Pistol)
-        else
+        // Ammo for sidearms - Example: 9 (for the .50 Pistol)
+        else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
             Draw.Dummy(Position.PrimaryAmmoDummy);
             Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), Position.SecondaryAmmo);
@@ -57,77 +56,50 @@ public class GGOHudScript : Script
         string PlayerIcon = Images.GetIcon(Images.Icon.Player);
         string PrimaryIcon = Images.GetIcon(Images.Icon.Primary);
         string SecondaryIcon = Images.GetIcon(Images.Icon.Secondary);
-
-        // Set of Icons
+        
         // Player Icon
-        if (File.Exists(PlayerIcon))
-        {
-            Draw.Texture(PlayerIcon, Position.PlayerIcon, Position.IconSize);
-        }
+        Draw.Texture(PlayerIcon, Position.PlayerIcon, Position.IconSize);
 
-        // Primary Icon
-        if (File.Exists(PrimaryIcon) && !Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
+        // Weapon information
+        // In order: Icon, Dummy, Weapon
+        if (Weapons.CurrentType() == Weapons.Type.Main)
         {
             Draw.Texture(PrimaryIcon, Position.PrimaryIcon, Position.IconSize);
+            Draw.Dummy(Position.SecondaryIconDummy);
+            Draw.Texture(WeaponImage, Position.PrimaryImage, Position.WeaponSize, true);
         }
-        else
-        {
-            Draw.Dummy(Position.PrimaryIconDummy);
-        }
-
-        // Secondary Weapon
-        if (File.Exists(SecondaryIcon) && Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
+        else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
             Draw.Texture(SecondaryIcon, Position.SecondaryIcon, Position.IconSize);
+            Draw.Dummy(Position.PrimaryIconDummy);
+            Draw.Texture(WeaponImage, Position.SecondaryImage, Position.WeaponSize, true);
         }
         else
         {
             Draw.Dummy(Position.SecondaryIconDummy);
-        }
-
-        // Second Row
-        // Picture for the primary weapon - Example: (see GUN_CarbineRifle.png for the Carbine)
-        if (File.Exists(WeaponImage) && !Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
-        {
-            Draw.Texture(WeaponImage, Position.PrimaryImage, Position.WeaponSize);
-        }
-
-        // Third Row
-        // Picture for the sidearm - Example: (see GUN_APPistol.png for the AP Pistol)
-        if (File.Exists(WeaponImage) && Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
-        {
-            Draw.Texture(WeaponImage, Position.SecondaryImage, Position.WeaponSize);
+            Draw.Dummy(Position.PrimaryIconDummy);
         }
     }
 
     public static void DrawShapesOnTick(object Sender, EventArgs Event)
     {
-        // Left Icons
-        // Player Info
+        // Icons
         Draw.Rectangle(Position.PlayerIconBG, Position.SquaredBG, Colors.Background);
-        // Primary Weapon
         Draw.Rectangle(Position.PrimaryIconBG, Position.SquaredBG, Colors.Background);
-        // Secondary Weapon
         Draw.Rectangle(Position.SecondaryIconBG, Position.SquaredBG, Colors.Background);
-
-        // First Row
+        
         // Player Info
         Draw.Rectangle(Position.PlayerInfoBG, Position.PlayerBGSize, Colors.Background);
-
-        // Second Row
-        // Primary Ammo
+        
+        // Ammo
         Draw.Rectangle(Position.PrimaryAmmoBG, Position.SquaredBG, Colors.Background);
-        // Primary Weapon
-        if (!Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
+        Draw.Rectangle(Position.SecondaryAmmoBG, Position.SquaredBG, Colors.Background);
+        // Weapons
+        if (Weapons.CurrentType() == Weapons.Type.Main)
         {
             Draw.Rectangle(Position.PrimaryBG, Position.WeaponBG, Colors.Background);
         }
-
-        // Third Row
-        // Secondary Ammo
-        Draw.Rectangle(Position.SecondaryAmmoBG, Position.SquaredBG, Colors.Background);
-        // Secondary Weapon
-        if (Checks.IsCurrentWeaponSidearm() && !Checks.IsCurrentWeaponBanned())
+        else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
             Draw.Rectangle(Position.SecondaryBG, Position.WeaponBG, Colors.Background);
         }
--- Weapons.cs
+++ Weapons.cs
@@ -1,11 +1,45 @@
-﻿using GTA.Native;
+﻿using GTA;
+using GTA.Native;
 using System.Collections.Generic;
 
 namespace GGOHud
 {
     class Weapons
     {
-        // Weapons that can be considered sidearm/secondary
+        /// <summary>
+        /// The type of the weapon that the player currently has.
+        /// </summary>
+        public enum Type
+        {
+            Banned = -1,
+            Main = 0,
+            Sidearm = 1,
+            Melee = 2
+        }
+
+        /// <summary>
+        /// Weapons that are not going to be shown on the HUD.
+        /// See the coments on the code to know why they are considered banned.
+        /// </summary>
+        public static readonly List<WeaponHash> Banned = new List<WeaponHash>
+        {
+            // Your hands are not firearms
+            WeaponHash.Unarmed,
+            // Flares are used to mark positions
+            WeaponHash.Flare,
+            // Recreational does not count
+            WeaponHash.Ball,
+            // If you enable the snow in-game they can be found everywhere
+            WeaponHash.Snowball,
+            // Improvised weapons should not be included
+            WeaponHash.Bottle,
+            // Is a standard flashlight, nothing that can kill
+            WeaponHash.Flashlight
+        };
+
+        /// <summary>
+        /// Weapons that can be considered sidearm either by the size or firing mechanism.
+        /// </summary>
         public static readonly List<WeaponHash> Sidearms = new List<WeaponHash> {
             // Melee
             WeaponHash.Knife,
@@ -45,15 +79,43 @@ namespace GGOHud
             // Heavy Weapons
             WeaponHash.CompactGrenadeLauncher
         };
-        // Weapons that are not going to be shown on the hud
-        public static readonly List<WeaponHash> Hidden = new List<WeaponHash>
-        {
-            WeaponHash.Unarmed,
-            WeaponHash.Flare,
-            WeaponHash.Ball,
-            WeaponHash.Snowball,
-            WeaponHash.Bottle,
-            WeaponHash.Flashlight
+
+        /// <summary>
+        /// Weapons that can be considered melee.
+        /// </summary>
+        public static readonly List<WeaponHash> Melee = new List<WeaponHash> {
+            WeaponHash.Knife,
+            WeaponHash.Nightstick,
+            WeaponHash.Hammer,
+            WeaponHash.Bat,
+            WeaponHash.Crowbar,
+            WeaponHash.GolfClub,
+            WeaponHash.Dagger,
+            WeaponHash.BattleAxe,
+            WeaponHash.KnuckleDuster,
         };
+
+        /// <summary>
+        /// Checks the weapon that the player currently has equiped.
+        /// </summary>
+        /// <returns>The Type of weapon.</returns>
+        public static Type CurrentType()
+        {
+            // Store the hash of the current weapon
+            WeaponHash CurrentWeapon = Game.Player.Character.Weapons.Current.Hash;
+
+            // If the weapon is Banned
+            if (Banned.Contains(CurrentWeapon))
+                return Type.Banned;
+            // If the weapon is not a firearm
+            if (Melee.Contains(CurrentWeapon))
+                return Type.Melee;
+            // If the weapon is a small firearm
+            if (Sidearms.Contains(CurrentWeapon))
+                return Type.Sidearm;
+            // If the weapon is other than the previous ones
+            else
+                return Type.Main;
+        }
     }
 }
