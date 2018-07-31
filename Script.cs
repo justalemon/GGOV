cb255fef54f6d8ebd44b0e021af084fbaee638d8
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-31 00:16:21 -0400

    Added configuration options for the UI elements

--- Script.cs
+++ Script.cs
@@ -28,26 +28,29 @@ public class GGOHudScript : Script
 
     public static void DrawTextOnTick(object Sender, EventArgs Event)
     {
+        Point PrimaryDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"), GUI.SizeFromConfig("AmmoDummy"));
+        Point SecondaryDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"), GUI.SizeFromConfig("AmmoDummy"));
+
         // Player name - Example: LLENN
-        Draw.Text(CharacterName, Position.PlayerName, 0.325f, false);
+        Draw.Text(CharacterName, GUI.PointFromConfig("PlayerName"), 0.325f, false);
 
         // Draw dummies if the weapon should not be shown
         if (Weapons.CurrentType() == Weapons.Type.Banned)
         {
-            Draw.Dummy(Position.PrimaryAmmoDummy);
-            Draw.Dummy(Position.SecondaryAmmoDummy);
+            Draw.Dummy(PrimaryDummy);
+            Draw.Dummy(SecondaryDummy);
         }
         // Ammo for primary weapons - Example: 30 (for the Bullpup Rifle)
         else if (Weapons.CurrentType() == Weapons.Type.Main)
         {
-            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), Position.PrimaryAmmo);
-            Draw.Dummy(Position.SecondaryAmmoDummy);
+            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"));
+            Draw.Dummy(SecondaryDummy);
         }
         // Ammo for sidearms - Example: 9 (for the .50 Pistol)
         else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
-            Draw.Dummy(Position.PrimaryAmmoDummy);
-            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), Position.SecondaryAmmo);
+            Draw.Dummy(PrimaryDummy);
+            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"));
         }
     }
 
@@ -58,52 +61,54 @@ public class GGOHudScript : Script
         string PlayerIcon = Images.GetIcon(Images.Icon.Player);
         string PrimaryIcon = Images.GetIcon(Images.Icon.Primary);
         string SecondaryIcon = Images.GetIcon(Images.Icon.Secondary);
-        
+        Point PrimaryDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconDummy"));
+        Point SecondaryDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconDummy"));
+
         // Player Icon
-        Draw.Texture(PlayerIcon, Position.PlayerIcon, Position.IconSize);
+        Draw.Texture(PlayerIcon, GUI.PointFromConfig("IconGenericX", "IconPlayerY"), GUI.SizeFromConfig("IconSize"));
 
         // Weapon information
         // In order: Icon, Dummy, Weapon
         if (Weapons.CurrentType() == Weapons.Type.Main)
         {
-            Draw.Texture(PrimaryIcon, Position.PrimaryIcon, Position.IconSize);
-            Draw.Dummy(Position.SecondaryIconDummy);
-            Draw.Texture(WeaponImage, Position.PrimaryImage, Position.WeaponSize, true);
+            Draw.Texture(PrimaryIcon, GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconSize"));
+            Draw.Dummy(SecondaryDummy);
+            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponPrimaryY"), GUI.SizeFromConfig("WeaponImage"), true);
         }
         else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
-            Draw.Texture(SecondaryIcon, Position.SecondaryIcon, Position.IconSize);
-            Draw.Dummy(Position.PrimaryIconDummy);
-            Draw.Texture(WeaponImage, Position.SecondaryImage, Position.WeaponSize, true);
+            Draw.Texture(SecondaryIcon, GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconSize"));
+            Draw.Dummy(PrimaryDummy);
+            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponSecondaryY"), GUI.SizeFromConfig("WeaponImage"), true);
         }
         else
         {
-            Draw.Dummy(Position.SecondaryIconDummy);
-            Draw.Dummy(Position.PrimaryIconDummy);
+            Draw.Dummy(PrimaryDummy);
+            Draw.Dummy(SecondaryDummy);
         }
     }
 
     public static void DrawShapesOnTick(object Sender, EventArgs Event)
     {
         // Icons
-        Draw.Rectangle(Position.PlayerIconBG, Position.SquaredBG, Colors.Background);
-        Draw.Rectangle(Position.PrimaryIconBG, Position.SquaredBG, Colors.Background);
-        Draw.Rectangle(Position.SecondaryIconBG, Position.SquaredBG, Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundPlayerY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundPrimaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundSecondaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
         
         // Player Info
-        Draw.Rectangle(Position.PlayerInfoBG, Position.PlayerBGSize, Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("PlayerBackground"), GUI.SizeFromConfig("PlayerBackground"), Colors.Background);
         
         // Ammo
-        Draw.Rectangle(Position.PrimaryAmmoBG, Position.SquaredBG, Colors.Background);
-        Draw.Rectangle(Position.SecondaryAmmoBG, Position.SquaredBG, Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("AmmoBackgroundX", "AmmoBackgroundPrimaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
+        Draw.Rectangle(GUI.PointFromConfig("AmmoBackgroundX", "AmmoBackgroundSecondaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
         // Weapons
         if (Weapons.CurrentType() == Weapons.Type.Main)
         {
-            Draw.Rectangle(Position.PrimaryBG, Position.WeaponBG, Colors.Background);
+            Draw.Rectangle(GUI.PointFromConfig("WeaponImageGenericX", "WeaponImagePrimaryY"), GUI.SizeFromConfig("WeaponBackground"), Colors.Background);
         }
         else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
-            Draw.Rectangle(Position.SecondaryBG, Position.WeaponBG, Colors.Background);
+            Draw.Rectangle(GUI.PointFromConfig("WeaponImageGenericX", "WeaponImageSecondaryY"), GUI.SizeFromConfig("WeaponBackground"), Colors.Background);
         }
     }
 
@@ -113,20 +118,20 @@ public class GGOHudScript : Script
         int Health = Function.Call<int>(Hash.GET_ENTITY_HEALTH, Game.Player.Character) - 100;
         int MaxHealth = Function.Call<int>(Hash.GET_ENTITY_MAX_HEALTH, Game.Player.Character) - 100;
         int HealthPercentage = Convert.ToInt32(((float)Health / MaxHealth) * 100f);
-        float Size = (Position.HealthBarS.Width / 100f) * HealthPercentage;
+        float Size = (GUI.SizeFromConfig("HealthBar").Width / 100f) * HealthPercentage;
 
         // Store the changes on a new object
-        Size BarSize = Position.HealthBarS;
+        Size BarSize = GUI.SizeFromConfig("HealthBar");
         BarSize.Width = Convert.ToInt32(Size);
         
         // First, draw the dividers so they are in the background
-        Draw.Rectangle(Position.HealthDividerOne, Position.HealthBarDivider, Colors.Healthy);
-        Draw.Rectangle(Position.HealthDividerTwo, Position.HealthBarDivider, Colors.Healthy);
-        Draw.Rectangle(Position.HealthDividerThree, Position.HealthBarDivider, Colors.Healthy);
-        Draw.Rectangle(Position.HealthDividerFour, Position.HealthBarDivider, Colors.Healthy);
+        Draw.Rectangle(GUI.PointFromConfig("HealthDividerOneX", "HealthDividerY"), GUI.SizeFromConfig("HealthDivider"), Colors.Healthy);
+        Draw.Rectangle(GUI.PointFromConfig("HealthDividerTwoX", "HealthDividerY"), GUI.SizeFromConfig("HealthDivider"), Colors.Healthy);
+        Draw.Rectangle(GUI.PointFromConfig("HealthDividerThreeX", "HealthDividerY"), GUI.SizeFromConfig("HealthDivider"), Colors.Healthy);
+        Draw.Rectangle(GUI.PointFromConfig("HealthDividerFourX", "HealthDividerY"), GUI.SizeFromConfig("HealthDivider"), Colors.Healthy);
 
         // Then, draw the bar by itself
-        Draw.Rectangle(Position.HealthBar, BarSize, Colors.FromHealth(MaxHealth, HealthPercentage));
+        Draw.Rectangle(GUI.PointFromConfig("HealthBar"), BarSize, Colors.FromHealth(MaxHealth, HealthPercentage));
     }
 
     public static void ChangeOnTick(object Sender, EventArgs Event)
