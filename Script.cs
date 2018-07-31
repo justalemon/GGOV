e28e0146bcc65ea57e4565691db9d1aa3667b9a0
Hannele Ruiz <justlemoncl@gmail.com>
2018-07-31 15:09:12 -0400

    Making everything fit on a single tick event

--- Script.cs
+++ Script.cs
@@ -13,107 +13,67 @@ public class GGOHudScript : Script
 
     public GGOHudScript()
     {
-        // Register the events
-        // TODO: See if having multiple events slow the script down
-        Tick += DrawTextOnTick;
-        Tick += DrawShapesOnTick;
-        Tick += DrawImagesOnTick;
-        Tick += DrawHealthOnTick;
-        Tick += ChangeOnTick;
+        // Register the event
+        Tick += OnTick;
         
         // If the Character Name on the config has changed, use it
         if (Config.GetValue("GGOHud", "CharacterName", "default") != "default")
             CharacterName = Config.GetValue("GGOHud", "CharacterName", "default");
     }
 
-    public static void DrawTextOnTick(object Sender, EventArgs Event)
+    public static void OnTick(object Sender, EventArgs Event)
     {
-        Point PrimaryDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"), GUI.SizeFromConfig("AmmoDummy"));
-        Point SecondaryDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"), GUI.SizeFromConfig("AmmoDummy"));
-
-        // Player name - Example: LLENN
-        Draw.Text(CharacterName, GUI.PointFromConfig("PlayerName"), 0.325f, false);
-
-        // Draw dummies if the weapon should not be shown
-        if (Weapons.CurrentType() == Weapons.Type.Banned)
-        {
-            Draw.Dummy(PrimaryDummy);
-            Draw.Dummy(SecondaryDummy);
-        }
-        // Ammo for primary weapons - Example: 30 (for the Bullpup Rifle)
-        else if (Weapons.CurrentType() == Weapons.Type.Main)
-        {
-            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"));
-            Draw.Dummy(SecondaryDummy);
-        }
-        // Ammo for sidearms - Example: 9 (for the .50 Pistol)
-        else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
-        {
-            Draw.Dummy(PrimaryDummy);
-            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"));
-        }
-    }
-
-    public static void DrawImagesOnTick(object Sender, EventArgs Event)
-    {
-        // Store the locations of the Icons here
+        // Store our temporary information here
+        Point PrimaryAmmoDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"), GUI.SizeFromConfig("AmmoDummy"));
+        Point SecondaryAmmoDummy = Point.Add(GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"), GUI.SizeFromConfig("AmmoDummy"));
+        Point PrimaryIconDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconDummy"));
+        Point SecondaryIconDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconDummy"));
         string WeaponImage = GUI.GetWeapon();
         string PlayerIcon = GUI.GetIcon(GUI.Icon.Player);
         string PrimaryIcon = GUI.GetIcon(GUI.Icon.Primary);
         string SecondaryIcon = GUI.GetIcon(GUI.Icon.Secondary);
-        Point PrimaryDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconDummy"));
-        Point SecondaryDummy = Point.Add(GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconDummy"));
 
+        // Player name
+        Draw.Text(CharacterName, GUI.PointFromConfig("PlayerName"), 0.325f, false);
         // Player Icon
         Draw.Texture(PlayerIcon, GUI.PointFromConfig("IconGenericX", "IconPlayerY"), GUI.SizeFromConfig("IconSize"));
-
-        // Weapon information
-        // In order: Icon, Dummy, Weapon
-        if (Weapons.CurrentType() == Weapons.Type.Main)
-        {
-            Draw.Texture(PrimaryIcon, GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconSize"));
-            Draw.Dummy(SecondaryDummy);
-            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponPrimaryY"), GUI.SizeFromConfig("WeaponImage"), true);
-        }
-        else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
-        {
-            Draw.Texture(SecondaryIcon, GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconSize"));
-            Draw.Dummy(PrimaryDummy);
-            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponSecondaryY"), GUI.SizeFromConfig("WeaponImage"), true);
-        }
-        else
-        {
-            Draw.Dummy(PrimaryDummy);
-            Draw.Dummy(SecondaryDummy);
-        }
-    }
-
-    public static void DrawShapesOnTick(object Sender, EventArgs Event)
-    {
-        // Icons
+        // Backgrounds
         Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundPlayerY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
         Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundPrimaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
         Draw.Rectangle(GUI.PointFromConfig("BackgroundGenericX", "BackgroundSecondaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
-        
-        // Player Info
         Draw.Rectangle(GUI.PointFromConfig("PlayerBackground"), GUI.SizeFromConfig("PlayerBackground"), Colors.Background);
-        
-        // Ammo
         Draw.Rectangle(GUI.PointFromConfig("AmmoBackgroundX", "AmmoBackgroundPrimaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
         Draw.Rectangle(GUI.PointFromConfig("AmmoBackgroundX", "AmmoBackgroundSecondaryY"), GUI.SizeFromConfig("SquaredBackground"), Colors.Background);
-        // Weapons
-        if (Weapons.CurrentType() == Weapons.Type.Main)
+
+        // Draw dummies if the weapon should not be shown
+        if (Weapons.CurrentType() == Weapons.Type.Banned)
         {
+            Draw.Dummy(PrimaryAmmoDummy);
+            Draw.Dummy(SecondaryAmmoDummy);
+            Draw.Dummy(PrimaryIconDummy);
+            Draw.Dummy(SecondaryIconDummy);
+        }
+        // Pimary weapon information
+        else if (Weapons.CurrentType() == Weapons.Type.Main)
+        {
+            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoPrimaryY"));
+            Draw.Dummy(SecondaryAmmoDummy);
+            Draw.Texture(PrimaryIcon, GUI.PointFromConfig("IconGenericX", "IconPrimaryY"), GUI.SizeFromConfig("IconSize"));
+            Draw.Dummy(SecondaryIconDummy);
+            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponPrimaryY"), GUI.SizeFromConfig("WeaponImage"), true);
             Draw.Rectangle(GUI.PointFromConfig("WeaponImageGenericX", "WeaponImagePrimaryY"), GUI.SizeFromConfig("WeaponBackground"), Colors.Background);
         }
+        // Secondary/Sidearm information
         else if (Weapons.CurrentType() == Weapons.Type.Sidearm)
         {
+            Draw.Dummy(PrimaryAmmoDummy);
+            Draw.Text(Game.Player.Character.Weapons.Current.AmmoInClip.ToString(), GUI.PointFromConfig("AmmoGenericX", "AmmoSecondaryY"));
+            Draw.Texture(SecondaryIcon, GUI.PointFromConfig("IconGenericX", "IconSecondaryY"), GUI.SizeFromConfig("IconSize"));
+            Draw.Dummy(PrimaryIconDummy);
+            Draw.Texture(WeaponImage, GUI.PointFromConfig("WeaponGenericX", "WeaponSecondaryY"), GUI.SizeFromConfig("WeaponImage"), true);
             Draw.Rectangle(GUI.PointFromConfig("WeaponImageGenericX", "WeaponImageSecondaryY"), GUI.SizeFromConfig("WeaponBackground"), Colors.Background);
         }
-    }
 
-    public static void DrawHealthOnTick(object Sender, EventArgs Event)
-    {
         // Calculate the bar size
         int Health = Function.Call<int>(Hash.GET_ENTITY_HEALTH, Game.Player.Character) - 100;
         int MaxHealth = Function.Call<int>(Hash.GET_ENTITY_MAX_HEALTH, Game.Player.Character) - 100;
@@ -132,11 +92,8 @@ public class GGOHudScript : Script
 
         // Then, draw the bar by itself
         Draw.Rectangle(GUI.PointFromConfig("HealthBar"), BarSize, Colors.FromHealth(MaxHealth, HealthPercentage));
-    }
 
-    public static void ChangeOnTick(object Sender, EventArgs Event)
-    {
-        // Disable the radar if is requested
+        // Disable the radar if the user want to
         if (Config.GetValue("GGOHud", "DisableRadarAndHUD", true))
         {
             Function.Call(Hash.HIDE_HUD_AND_RADAR_THIS_FRAME);
