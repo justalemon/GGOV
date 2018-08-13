63165258fcb3ea8cb78a7d7041b195b2d4823a42
Hannele Ruiz <justlemoncl@gmail.com>
2018-08-13 14:55:30 -0300

    Added more Character Names

--- Data.cs
+++ Data.cs
@@ -52,12 +52,36 @@ namespace GGOHud
         /// </summary>
         public static Dictionary<int, string> Names = new Dictionary<int, string>
         {
-            { -597926235, "Cheng" },
-            { 2089096292, "Translator" },
+            // Main Missions
+            { 1830688247, "Amanda" },
+            { -1111799518, "Brad" },
+            { -520477356, "Casey" },
             { 1240128502, "Chef" },
+            { 365775923, "Dave" },
+            { -1674727288, "Dom" },
+            { 351016938, "Chop" },
+            { -1313761614, "Floyd" },
+            { -1692214353, "Franklin" },
+            { 1459905209, "Jimmy" },
+            { 1302784073, "Lester" },
             { 1706635382, "Lamar" },
+            { 225514697, "Michael" },
+            { -304305299, "Mr K" },
+            { -1124046095, "Ron" },
             { 915948376, "Stretch" },
-            { -1643617475, "A. Owner" }
+            { -566941131, "Tracy" },
+            { -597926235, "Tao Cheng" },
+            { 2089096292, "Translator" },
+            { -1686040670, "Trevor" },
+            { -1835459726, "Wade" },
+            // Heists
+            // Missing Ones: Karim, Gus, Norm, McReary, Daryl, Hugh, Karl, Feltz
+            { 712602007, "Eddie" },
+            { 994527967, "Eddie" },
+            { -409745176, "Taliana" },
+            { 1209091352, "Norm" },
+            { 357551935, "Paige" },
+            { 1401530684, "Feltz" },
         };
     }
 }
