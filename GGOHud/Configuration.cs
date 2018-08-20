c1d336b83fae3a6af6dcb0e45d27255a9151c577
Hannele Ruiz <justlemoncl@gmail.com>
2018-08-19 23:44:33 -0300

    Added configuration system

---
+++ GGOHud/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using GTA;
+
+namespace GGOHud
+{
+    public class Configuration
+    {
+        /// <summary>
+        /// The raw ScriptSettings instance.
+        /// </summary>
+        private ScriptSettings RawSettings;
+        /// <summary>
+        /// The base for the config (aka the base element).
+        /// </summary>
+        private string ConfigBase;
+        /// <summary>
+        /// If the debug mode should be enabled.
+        /// </summary>
+        public bool Debug
+        {
+            get
+            {
+                return RawSettings.GetValue(ConfigBase, "Debug", false);
+            }
+        }
+
+        public Configuration(string ConfigFile, string Base)
+        {
+            RawSettings = ScriptSettings.Load(ConfigFile);
+            ConfigBase = Base;
+        }
+    }
+}
--- GGOHud/Main.cs
+++ GGOHud/Main.cs
@@ -4,6 +4,14 @@ namespace GGOHud
 {
     public class GGOHud : Script
     {
+        private Configuration Config = new Configuration("scripts\\GGOHud.ini", "GGOHud");
 
+        public GGOHud()
+        {
+            if (Config.Debug)
+            {
+                UI.Notify("GGOHud has been enabled.");
+            }
+        }
     }
 }
