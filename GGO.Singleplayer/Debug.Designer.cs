50670852140a4d63a9e077a8abad2cfd7540d03c
Hannele Ruiz <justlemoncl@gmail.com>
2018-10-17 08:57:58 -0300

    Removing some of the duplicated elements

--- GGO.Singleplayer/Debug.Designer.cs
+++ GGO.Singleplayer/Debug.Designer.cs
@@ -33,6 +33,9 @@
             this.PlayerName = new System.Windows.Forms.Label();
             this.Version = new System.Windows.Forms.Label();
             this.SizesPositions = new System.Windows.Forms.GroupBox();
+            this.AmmoPosition = new System.Windows.Forms.Label();
+            this.WeaponImageSize = new System.Windows.Forms.Label();
+            this.PlayerHealthPosition = new System.Windows.Forms.Label();
             this.PlayerHealthSize = new System.Windows.Forms.Label();
             this.PlayerInfoSize = new System.Windows.Forms.Label();
             this.PlayerPosition = new System.Windows.Forms.Label();
@@ -48,12 +51,6 @@
             this.DividerSize = new System.Windows.Forms.Label();
             this.ElementsRelative = new System.Windows.Forms.Label();
             this.InfoStrip = new System.Windows.Forms.StatusStrip();
-            this.PlayerPosition = new System.Windows.Forms.Label();
-            this.PlayerInfoSize = new System.Windows.Forms.Label();
-            this.PlayerHealthSize = new System.Windows.Forms.Label();
-            this.PlayerHealthPosition = new System.Windows.Forms.Label();
-            this.WeaponImageSize = new System.Windows.Forms.Label();
-            this.AmmoPosition = new System.Windows.Forms.Label();
             this.GeneralGroup.SuspendLayout();
             this.SizesPositions.SuspendLayout();
             this.SuspendLayout();
@@ -63,11 +60,9 @@
             this.GeneralGroup.Controls.Add(this.HudDisabled);
             this.GeneralGroup.Controls.Add(this.PlayerName);
             this.GeneralGroup.Controls.Add(this.Version);
-            this.GeneralGroup.Location = new System.Drawing.Point(18, 18);
-            this.GeneralGroup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GeneralGroup.Location = new System.Drawing.Point(12, 12);
             this.GeneralGroup.Name = "GeneralGroup";
-            this.GeneralGroup.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.GeneralGroup.Size = new System.Drawing.Size(428, 97);
+            this.GeneralGroup.Size = new System.Drawing.Size(285, 63);
             this.GeneralGroup.TabIndex = 0;
             this.GeneralGroup.TabStop = false;
             this.GeneralGroup.Text = "General Information";
@@ -75,30 +70,27 @@
             // HudDisabled
             // 
             this.HudDisabled.AutoSize = true;
-            this.HudDisabled.Location = new System.Drawing.Point(9, 65);
-            this.HudDisabled.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HudDisabled.Location = new System.Drawing.Point(6, 42);
             this.HudDisabled.Name = "HudDisabled";
-            this.HudDisabled.Size = new System.Drawing.Size(101, 20);
+            this.HudDisabled.Size = new System.Drawing.Size(68, 13);
             this.HudDisabled.TabIndex = 2;
             this.HudDisabled.Text = "HudDisabled";
             // 
             // PlayerName
             // 
             this.PlayerName.AutoSize = true;
-            this.PlayerName.Location = new System.Drawing.Point(9, 45);
-            this.PlayerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PlayerName.Location = new System.Drawing.Point(6, 29);
             this.PlayerName.Name = "PlayerName";
-            this.PlayerName.Size = new System.Drawing.Size(51, 20);
+            this.PlayerName.Size = new System.Drawing.Size(35, 13);
             this.PlayerName.TabIndex = 1;
             this.PlayerName.Text = "Name";
             // 
             // Version
             // 
             this.Version.AutoSize = true;
-            this.Version.Location = new System.Drawing.Point(9, 25);
-            this.Version.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Version.Location = new System.Drawing.Point(6, 16);
             this.Version.Name = "Version";
-            this.Version.Size = new System.Drawing.Size(63, 20);
+            this.Version.Size = new System.Drawing.Size(42, 13);
             this.Version.TabIndex = 0;
             this.Version.Text = "Version";
             // 
@@ -121,28 +113,54 @@
             this.SizesPositions.Controls.Add(this.DividerPosition);
             this.SizesPositions.Controls.Add(this.DividerSize);
             this.SizesPositions.Controls.Add(this.ElementsRelative);
-            this.SizesPositions.Location = new System.Drawing.Point(18, 125);
-            this.SizesPositions.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SizesPositions.Location = new System.Drawing.Point(12, 81);
             this.SizesPositions.Name = "SizesPositions";
-            this.SizesPositions.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.SizesPositions.Size = new System.Drawing.Size(428, 469);
+            this.SizesPositions.Size = new System.Drawing.Size(285, 305);
             this.SizesPositions.TabIndex = 1;
             this.SizesPositions.TabStop = false;
             this.SizesPositions.Text = "Sizes and Positions";
             // 
+            // AmmoPosition
+            // 
+            this.AmmoPosition.AutoSize = true;
+            this.AmmoPosition.Location = new System.Drawing.Point(6, 265);
+            this.AmmoPosition.Name = "AmmoPosition";
+            this.AmmoPosition.Size = new System.Drawing.Size(73, 13);
+            this.AmmoPosition.TabIndex = 18;
+            this.AmmoPosition.Text = "AmmoPosition";
+            // 
+            // WeaponImageSize
+            // 
+            this.WeaponImageSize.AutoSize = true;
+            this.WeaponImageSize.Location = new System.Drawing.Point(5, 252);
+            this.WeaponImageSize.Name = "WeaponImageSize";
+            this.WeaponImageSize.Size = new System.Drawing.Size(97, 13);
+            this.WeaponImageSize.TabIndex = 17;
+            this.WeaponImageSize.Text = "WeaponImageSize";
+            // 
+            // PlayerHealthPosition
+            // 
+            this.PlayerHealthPosition.AutoSize = true;
+            this.PlayerHealthPosition.Location = new System.Drawing.Point(6, 233);
+            this.PlayerHealthPosition.Name = "PlayerHealthPosition";
+            this.PlayerHealthPosition.Size = new System.Drawing.Size(85, 13);
+            this.PlayerHealthPosition.TabIndex = 16;
+            this.PlayerHealthPosition.Text = "PlayerHealthPos";
+            // 
             // PlayerHealthSize
             // 
             this.PlayerHealthSize.AutoSize = true;
-            this.PlayerHealthSize.Location = new System.Drawing.Point(6, 224);
+            this.PlayerHealthSize.Location = new System.Drawing.Point(6, 220);
             this.PlayerHealthSize.Name = "PlayerHealthSize";
             this.PlayerHealthSize.Size = new System.Drawing.Size(87, 13);
-            this.PlayerHealthSize.TabIndex = 16;
+            this.PlayerHealthSize.TabIndex = 15;
             this.PlayerHealthSize.Text = "PlayerHealthSize";
             // 
             // PlayerInfoSize
             // 
             this.PlayerInfoSize.AutoSize = true;
-            this.PlayerInfoSize.Location = new System.Drawing.Point(6, 211);
+            this.PlayerInfoSize.Location = new System.Drawing.Point(6, 185);
+            this.PlayerInfoSize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.PlayerInfoSize.Name = "PlayerInfoSize";
             this.PlayerInfoSize.Size = new System.Drawing.Size(74, 13);
             this.PlayerInfoSize.TabIndex = 15;
@@ -154,198 +172,125 @@
             this.PlayerPosition.Location = new System.Drawing.Point(6, 198);
             this.PlayerPosition.Name = "PlayerPosition";
             this.PlayerPosition.Size = new System.Drawing.Size(73, 13);
-            this.PlayerPosition.TabIndex = 14;
+            this.PlayerPosition.TabIndex = 13;
             this.PlayerPosition.Text = "PlayerPosition";
             // 
             // SquadHealthPos
             // 
             this.SquadHealthPos.AutoSize = true;
-            this.SquadHealthPos.Location = new System.Drawing.Point(9, 265);
-            this.SquadHealthPos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SquadHealthPos.Location = new System.Drawing.Point(6, 172);
             this.SquadHealthPos.Name = "SquadHealthPos";
-            this.SquadHealthPos.Size = new System.Drawing.Size(130, 20);
+            this.SquadHealthPos.Size = new System.Drawing.Size(87, 13);
             this.SquadHealthPos.TabIndex = 12;
             this.SquadHealthPos.Text = "SquadHealthPos";
             // 
             // SquadHealthSize
             // 
             this.SquadHealthSize.AutoSize = true;
-            this.SquadHealthSize.Location = new System.Drawing.Point(9, 245);
-            this.SquadHealthSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SquadHealthSize.Location = new System.Drawing.Point(6, 159);
             this.SquadHealthSize.Name = "SquadHealthSize";
-            this.SquadHealthSize.Size = new System.Drawing.Size(134, 20);
+            this.SquadHealthSize.Size = new System.Drawing.Size(89, 13);
             this.SquadHealthSize.TabIndex = 11;
             this.SquadHealthSize.Text = "SquadHealthSize";
             // 
             // SquadInfoSize
             // 
             this.SquadInfoSize.AutoSize = true;
-            this.SquadInfoSize.Location = new System.Drawing.Point(9, 225);
-            this.SquadInfoSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SquadInfoSize.Location = new System.Drawing.Point(6, 146);
             this.SquadInfoSize.Name = "SquadInfoSize";
-            this.SquadInfoSize.Size = new System.Drawing.Size(115, 20);
+            this.SquadInfoSize.Size = new System.Drawing.Size(76, 13);
             this.SquadInfoSize.TabIndex = 10;
             this.SquadInfoSize.Text = "SquadInfoSize";
             // 
             // SquadPosition
             // 
             this.SquadPosition.AutoSize = true;
-            this.SquadPosition.Location = new System.Drawing.Point(9, 205);
-            this.SquadPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SquadPosition.Location = new System.Drawing.Point(6, 133);
             this.SquadPosition.Name = "SquadPosition";
-            this.SquadPosition.Size = new System.Drawing.Size(112, 20);
+            this.SquadPosition.Size = new System.Drawing.Size(75, 13);
             this.SquadPosition.TabIndex = 9;
             this.SquadPosition.Text = "SquadPosition";
             // 
             // IconPosition
             // 
             this.IconPosition.AutoSize = true;
-            this.IconPosition.Location = new System.Drawing.Point(9, 165);
-            this.IconPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.IconPosition.Location = new System.Drawing.Point(6, 107);
             this.IconPosition.Name = "IconPosition";
-            this.IconPosition.Size = new System.Drawing.Size(96, 20);
+            this.IconPosition.Size = new System.Drawing.Size(65, 13);
             this.IconPosition.TabIndex = 7;
             this.IconPosition.Text = "IconPosition";
             // 
             // IconBackgroundSize
             // 
             this.IconBackgroundSize.AutoSize = true;
-            this.IconBackgroundSize.Location = new System.Drawing.Point(9, 145);
-            this.IconBackgroundSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.IconBackgroundSize.Location = new System.Drawing.Point(6, 94);
             this.IconBackgroundSize.Name = "IconBackgroundSize";
-            this.IconBackgroundSize.Size = new System.Drawing.Size(157, 20);
+            this.IconBackgroundSize.Size = new System.Drawing.Size(106, 13);
             this.IconBackgroundSize.TabIndex = 6;
             this.IconBackgroundSize.Text = "IconBackgroundSize";
             // 
             // IconImageSize
             // 
             this.IconImageSize.AutoSize = true;
-            this.IconImageSize.Location = new System.Drawing.Point(9, 125);
-            this.IconImageSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.IconImageSize.Location = new System.Drawing.Point(6, 81);
             this.IconImageSize.Name = "IconImageSize";
-            this.IconImageSize.Size = new System.Drawing.Size(116, 20);
+            this.IconImageSize.Size = new System.Drawing.Size(77, 13);
             this.IconImageSize.TabIndex = 5;
             this.IconImageSize.Text = "IconImageSize";
             // 
             // NamePosition
             // 
             this.NamePosition.AutoSize = true;
-            this.NamePosition.Location = new System.Drawing.Point(9, 85);
-            this.NamePosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NamePosition.Location = new System.Drawing.Point(6, 55);
             this.NamePosition.Name = "NamePosition";
-            this.NamePosition.Size = new System.Drawing.Size(107, 20);
+            this.NamePosition.Size = new System.Drawing.Size(72, 13);
             this.NamePosition.TabIndex = 3;
             this.NamePosition.Text = "NamePosition";
             // 
             // DividerPosition
             // 
             this.DividerPosition.AutoSize = true;
-            this.DividerPosition.Location = new System.Drawing.Point(9, 65);
-            this.DividerPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DividerPosition.Location = new System.Drawing.Point(6, 42);
             this.DividerPosition.Name = "DividerPosition";
-            this.DividerPosition.Size = new System.Drawing.Size(113, 20);
+            this.DividerPosition.Size = new System.Drawing.Size(77, 13);
             this.DividerPosition.TabIndex = 2;
             this.DividerPosition.Text = "DividerPosition";
             // 
             // DividerSize
             // 
             this.DividerSize.AutoSize = true;
-            this.DividerSize.Location = new System.Drawing.Point(9, 45);
-            this.DividerSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DividerSize.Location = new System.Drawing.Point(6, 29);
             this.DividerSize.Name = "DividerSize";
-            this.DividerSize.Size = new System.Drawing.Size(88, 20);
+            this.DividerSize.Size = new System.Drawing.Size(60, 13);
             this.DividerSize.TabIndex = 1;
             this.DividerSize.Text = "DividerSize";
             // 
             // ElementsRelative
             // 
             this.ElementsRelative.AutoSize = true;
-            this.ElementsRelative.Location = new System.Drawing.Point(9, 25);
-            this.ElementsRelative.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ElementsRelative.Location = new System.Drawing.Point(6, 16);
             this.ElementsRelative.Name = "ElementsRelative";
-            this.ElementsRelative.Size = new System.Drawing.Size(133, 20);
+            this.ElementsRelative.Size = new System.Drawing.Size(89, 13);
             this.ElementsRelative.TabIndex = 0;
             this.ElementsRelative.Text = "ElementsRelative";
             // 
             // InfoStrip
             // 
             this.InfoStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.InfoStrip.Location = new System.Drawing.Point(0, 610);
+            this.InfoStrip.Location = new System.Drawing.Point(0, 389);
             this.InfoStrip.Name = "InfoStrip";
-            this.InfoStrip.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
-            this.InfoStrip.Size = new System.Drawing.Size(464, 22);
+            this.InfoStrip.Size = new System.Drawing.Size(309, 22);
             this.InfoStrip.TabIndex = 2;
-            // 
-            // PlayerPosition
-            // 
-            this.PlayerPosition.AutoSize = true;
-            this.PlayerPosition.Location = new System.Drawing.Point(9, 298);
-            this.PlayerPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.PlayerPosition.Name = "PlayerPosition";
-            this.PlayerPosition.Size = new System.Drawing.Size(108, 20);
-            this.PlayerPosition.TabIndex = 13;
-            this.PlayerPosition.Text = "PlayerPosition";
-            // 
-            // PlayerInfoSize
-            // 
-            this.PlayerInfoSize.AutoSize = true;
-            this.PlayerInfoSize.Location = new System.Drawing.Point(9, 318);
-            this.PlayerInfoSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.PlayerInfoSize.Name = "PlayerInfoSize";
-            this.PlayerInfoSize.Size = new System.Drawing.Size(111, 20);
-            this.PlayerInfoSize.TabIndex = 14;
-            this.PlayerInfoSize.Text = "PlayerInfoSize";
-            // 
-            // PlayerHealthSize
-            // 
-            this.PlayerHealthSize.AutoSize = true;
-            this.PlayerHealthSize.Location = new System.Drawing.Point(9, 338);
-            this.PlayerHealthSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.PlayerHealthSize.Name = "PlayerHealthSize";
-            this.PlayerHealthSize.Size = new System.Drawing.Size(130, 20);
-            this.PlayerHealthSize.TabIndex = 15;
-            this.PlayerHealthSize.Text = "PlayerHealthSize";
-            // 
-            // PlayerHealthPosition
-            // 
-            this.PlayerHealthPosition.AutoSize = true;
-            this.PlayerHealthPosition.Location = new System.Drawing.Point(9, 358);
-            this.PlayerHealthPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.PlayerHealthPosition.Name = "PlayerHealthPosition";
-            this.PlayerHealthPosition.Size = new System.Drawing.Size(126, 20);
-            this.PlayerHealthPosition.TabIndex = 16;
-            this.PlayerHealthPosition.Text = "PlayerHealthPos";
-            // 
-            // WeaponImageSize
-            // 
-            this.WeaponImageSize.AutoSize = true;
-            this.WeaponImageSize.Location = new System.Drawing.Point(8, 388);
-            this.WeaponImageSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.WeaponImageSize.Name = "WeaponImageSize";
-            this.WeaponImageSize.Size = new System.Drawing.Size(145, 20);
-            this.WeaponImageSize.TabIndex = 17;
-            this.WeaponImageSize.Text = "WeaponImageSize";
-            // 
-            // AmmoPosition
-            // 
-            this.AmmoPosition.AutoSize = true;
-            this.AmmoPosition.Location = new System.Drawing.Point(9, 408);
-            this.AmmoPosition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.AmmoPosition.Name = "AmmoPosition";
-            this.AmmoPosition.Size = new System.Drawing.Size(111, 20);
-            this.AmmoPosition.TabIndex = 18;
-            this.AmmoPosition.Text = "AmmoPosition";
             // 
             // Debug
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 632);
+            this.ClientSize = new System.Drawing.Size(309, 411);
             this.Controls.Add(this.InfoStrip);
             this.Controls.Add(this.SizesPositions);
             this.Controls.Add(this.GeneralGroup);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Debug";
