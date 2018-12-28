1bcb02bb1bb690ee8ef27f24a242c05d31607ad7
Hannele Ruiz <justlemoncl@gmail.com>
2018-12-28 00:36:25 -0300

    Added basic inventory window (just the name and the background)

---
+++ GGO/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using GTA;
+using System;
+using System.Drawing;
+
+namespace GGO
+{
+    public static class Inventory
+    {
+        /// <summary>
+        /// Draws the Inventory on screen.
+        /// </summary>
+        public static void Draw()
+        {
+            UIRectangle GeneralBackground = new UIRectangle(GGO.Config.InventoryBackgroundPosition, GGO.Config.InventoryBackgroundSize, Colors.Inventory);
+            GeneralBackground.Draw();
+            UIRectangle InfoBackground = new UIRectangle(GGO.Config.InventoryBackgroundPosition, GGO.Config.InventoryInfoSize, Colors.Backgrounds);
+            InfoBackground.Draw();
+            UIRectangle NameBackground = new UIRectangle(GGO.Config.InventoryColourPosition, GGO.Config.InventoryColourSize, Colors.Details);
+            NameBackground.Draw();
+            UIText PlayerName = new UIText(Game.Player.Name, GGO.Config.InventoryPlayerName, 0.7f, Color.White, GTA.Font.Monospace, false, false, false);
+            PlayerName.Draw();
+        }
+    }
+}
