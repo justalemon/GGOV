b413695fa0cbe8cd126d14cabf728ae6341f7c7f
Hannele Ruiz <justlemoncl@gmail.com>
2018-12-28 23:35:44 -0300

    Added inventory rectangle

--- GGO/Properties/Resources.Designer.cs
+++ GGO/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace GGO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap InventoryItem {
+            get {
+                object obj = ResourceManager.GetObject("InventoryItem", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap NoWeapon {
             get {
                 object obj = ResourceManager.GetObject("NoWeapon", resourceCulture);
