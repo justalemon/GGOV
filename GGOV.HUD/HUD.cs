5297a9fe963d11642d1385c217596d66684c21e1
Hannele Ruiz <justlemoncl@gmail.com>
2020-11-05 20:49:01 -0300

    Added simple script for drawing the HUD

---
+++ GGOV.HUD/HUD.cs
@@ -0,0 +1,37 @@
+﻿using GTA;
+using LemonUI;
+using System;
+
+namespace GGO
+{
+    /// <summary>
+    /// Script that handles the HUD Elements.
+    /// </summary>
+    public class HUD : Script
+    {
+        #region Private Fields
+
+        private readonly ObjectPool pool = new ObjectPool();
+
+        #endregion
+
+        #region Constructor
+
+        public HUD()
+        {
+            Tick += HUD_Tick;
+        }
+
+        #endregion
+
+        #region 
+
+        private void HUD_Tick(object sender, EventArgs e)
+        {
+            // Just process the HUD Elements
+            pool.Process();
+        }
+
+        #endregion
+    }
+}
