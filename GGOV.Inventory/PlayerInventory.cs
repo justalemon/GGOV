dd574fc29309574fd6c0e89ce85e045540c7a74c
Hannele Ruiz <justlemoncl@gmail.com>
2020-11-22 06:29:18 -0300

    Added basics for the Inventory script

---
+++ GGOV.Inventory/PlayerInventory.cs
@@ -0,0 +1,48 @@
+﻿using GTA;
+using LemonUI;
+using PlayerCompanion;
+using System;
+
+namespace GGO
+{
+    /// <summary>
+    /// The Inventory UI Item.
+    /// </summary>
+    public class PlayerInventory : IRecalculable, IProcessable
+    {
+        #region Properties
+
+        /// <summary>
+        /// If the inventory is visible or not.
+        /// </summary>
+        public bool Visible { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        internal PlayerInventory()
+        {
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Recalculates the position of the menu.
+        /// </summary>
+        public void Recalculate()
+        {
+        }
+        /// <summary>
+        /// Processes the inventory.
+        /// </summary>
+        public void Process()
+        {
+
+        }
+
+        #endregion
+    }
+}
