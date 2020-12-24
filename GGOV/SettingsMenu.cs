2f744ada7acfac3d2c1e93a1d05478312500f2c2
Hannele Ruiz <justlemoncl@gmail.com>
2020-12-24 15:04:53 -0300

    Reorganized the position of the items on the screen

--- GGOV/SettingsMenu.cs
+++ GGOV/SettingsMenu.cs
@@ -11,6 +11,12 @@ namespace GGO
     {
         [JsonProperty("equip")]
         public bool Equip { get; set; } = true;
+        [JsonProperty("deatnmarkers")]
+        public bool DeathMarkers { get; set; } = true;
+        [JsonProperty("inventory")]
+        public bool Inventory { get; set; } = true;
+        [JsonProperty("squad")]
+        public bool Squad { get; set; } = true;
         [JsonProperty("squadx")]
         public float SquadX { get; set; } = 0;
         [JsonProperty("squady")]
@@ -19,12 +25,6 @@ namespace GGO
         public float PlayerX { get; set; } = 0;
         [JsonProperty("playery")]
         public float PlayerY { get; set; } = 0;
-        [JsonProperty("deatnmarkers")]
-        public bool DeathMarkers { get; set; } = true;
-        [JsonProperty("inventory")]
-        public bool Inventory { get; set; } = true;
-        [JsonProperty("squad")]
-        public bool Squad { get; set; } = true;
     }
 
     /// <summary>
@@ -36,13 +36,13 @@ namespace GGO
 
         private static readonly string path = Path.Combine(GGO.location, "GGOV", "Config.json");
         public NativeCheckboxItem EquipWeapons { get; } = new NativeCheckboxItem("Equip Inventory Weapons", "Enabled: Marks the Weapons as Primary or Secondary, allowing you to switch between your Primary, Secondary and Fists with 1, 2 and 3 respectively (like Apex Legends).~n~~n~Disabled: Equips the Weapons directly. Requires you to open the Inventory every time that you want to change weapons.", true);
+        public NativeCheckboxItem DeathMarkers { get; } = new NativeCheckboxItem("Enable Death Markers", "Enables the Death Markers shown when the Peds around you die.", true);
+        public NativeCheckboxItem Inventory { get; } = new NativeCheckboxItem("Use Inventory", "Enables or Disables the Inventory system.", true);
+        public NativeCheckboxItem Squad { get; } = new NativeCheckboxItem("Show Squad Members", "If the Squad Members (top left) should be shown on the screen.", true);
         public FloatSelectorItem SquadX { get; } = new FloatSelectorItem("Squad Members: X", "The X value of the Squad Health Information.", 103);
         public FloatSelectorItem SquadY { get; } = new FloatSelectorItem("Squad Members: Y", "The Y value of the Squad Health Information.", 66);
         public FloatSelectorItem PlayerX { get; } = new FloatSelectorItem("Player Info: X", "The X value of the Player Information.", -388);
         public FloatSelectorItem PlayerY { get; } = new FloatSelectorItem("Player Info: Y", "The Y value of the Player Information.", -232);
-        public NativeCheckboxItem DeathMarkers { get; } = new NativeCheckboxItem("Enable Death Markers", "Enables the Death Markers shown when the Peds around you die.", true);
-        public NativeCheckboxItem Inventory { get; } = new NativeCheckboxItem("Use Inventory", "Enables or Disables the Inventory system.", true);
-        public NativeCheckboxItem Squad { get; } = new NativeCheckboxItem("Show Squad Members", "If the Squad Members (top left) should be shown on the screen.", true);
         public NativeItem Save { get; } = new NativeItem("Save", "Saves all of the current settings.");
 
         #endregion
@@ -58,13 +58,13 @@ namespace GGO
                 Configuration config = JsonConvert.DeserializeObject<Configuration>(contents);
 
                 EquipWeapons.Checked = config.Equip;
+                DeathMarkers.Checked = config.DeathMarkers;
+                Inventory.Checked = config.Inventory;
+                Squad.Checked = config.Squad;
                 SquadX.SelectedItem = config.SquadX;
                 SquadY.SelectedItem = config.SquadY;
                 PlayerX.SelectedItem = config.PlayerX;
                 PlayerY.SelectedItem = config.PlayerY;
-                DeathMarkers.Checked = config.DeathMarkers;
-                Inventory.Checked = config.Inventory;
-                Squad.Checked = config.Squad;
             }
 
             // Set the options of the menu
@@ -72,13 +72,13 @@ namespace GGO
             ResetCursorWhenOpened = false;
             // Add the items and submenus
             Add(EquipWeapons);
+            Add(DeathMarkers);
+            Add(Inventory);
+            Add(Squad);
             Add(SquadX);
             Add(SquadY);
             Add(PlayerX);
             Add(PlayerY);
-            Add(DeathMarkers);
-            Add(Inventory);
-            Add(Squad);
             Add(Save);
             // Subscribe the events
             EquipWeapons.CheckboxChanged += EquipWeapons_CheckboxChanged;
@@ -108,13 +108,13 @@ namespace GGO
             Configuration config = new Configuration
             {
                 Equip = EquipWeapons.Checked,
+                DeathMarkers = DeathMarkers.Checked,
+                Inventory = Inventory.Checked,
+                Squad = Squad.Checked,
                 SquadX = SquadX.SelectedItem,
                 SquadY = SquadY.SelectedItem,
                 PlayerX = PlayerX.SelectedItem,
                 PlayerY = PlayerY.SelectedItem,
-                DeathMarkers = DeathMarkers.Checked,
-                Inventory = Inventory.Checked,
-                Squad = Squad.Checked,
             };
 
             string contents = JsonConvert.SerializeObject(config);
